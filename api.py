c407a3c25b8c0b95ac92ae489ca78b13804f422b
Jeremías San Martín <jerexxypunto@gmail.com>
2023-09-28 14:44:14 -0300

    Create course config and generate prompt for course setting.

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
-import json
-
 from flask import Flask, request
+
+from langchain.prompts import PromptTemplate
+
 app = Flask(__name__)
 
 @app.route('/process', methods=['POST'])
@@ -12,8 +13,6 @@ def process():
     return 'OK', 200
 
 
-from your_module import crearFirstPrompt  # Asegúrate de que esta ruta de importación sea correcta
-
 @app.route('/chatdev', methods=['POST'])
 def aidi_create():
     body = request.json
@@ -23,13 +22,41 @@ def aidi_create():
 
     print(body)
     
+    # Crear un course config
     if target == "course-setting" and action == "create":
-        crearFirstPrompt()
+         course_setting_generate(contentido)
+         print("Crea propt")
+        #crearFirstPrompt()
 
     return 'OK', 200
-    
 
 
-    return 'OK', 200
+def course_setting_generate(prompt):
+    template = """
+          El curso trata sobre {de_que_trata}.
+          Los participantes de curso y sus metas son: {participantes_y_metas}.
+          Los conocmientos necesarios antes de tomar el curso son: {conocmientos_base}.
+          El curso se creare en el idioma: {idioma}.
+          La modalidad del curso sera: {modalidad}.
+
+          ¿Optimiza de forma masiva el aprendisaje en la plataforma? {mooc}.
+          ¿Optimiza el curso para aprendizaje basado en grupos pequeño? {project_bases}.
+          ¿Generar prototipos de tareas para posterior elaboracion? {task_prototype}.
+
+          El curso durara {weeks} semanas.
+          El nivel del curso sera {course_level}
+          """
+    res = PromptTemplate.from_template(template)
+
+    res.format(adjetive="de_que_trata", content=prompt["de_que_trata"])
+    res.format(adjetive="participantes_y_metas", content=prompt["participantes_y_metas"])
+    res.format(adjetive="conocmientos_base", content=prompt["conocmientos_base"])
+    res.format(adjetive="idioma", content=prompt["idioma"])
+    res.format(adjetive="mooc", content=prompt["mooc"])
+    res.format(adjetive="project_bases", content=prompt["project_bases"])
+    res.format(adjetive="task_prototype", content=prompt["task_prototype"])
+    res.format(adjetive="weeks", content=prompt["weeks"])
+    res.format(adjetive="course_level", content=prompt["course_level"])
 
 
+    return res
