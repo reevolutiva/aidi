143408cc89383b5e217454b541ae0c98df0799f3
Jeremías San Martín <jerexxypunto@gmail.com>
2023-09-29 16:15:52 -0300

    Refactored code to use a function to create a prompt chat dev command.

--- api.py
+++ api.py
@@ -1,7 +1,5 @@
 from flask import Flask, request
-
-from langchain.prompts import PromptTemplate
-
+from langchain import PromptTemplate
 import os
 
 app = Flask(__name__)
@@ -13,16 +11,17 @@ def process():
  
     body = request.json
 
-    config = body["config"]
-    org = body["org"]
-    task = body["task"]
-    name = body["name"]   
+    res = course_setting_generate(body)
+
+    comand = create_prompt_chat_dev( "chatDevCore.py" , "ReevolutivaResumen" , {"name":"DesingInstrutional","task":res}) 
+
+    print(comand)
    
-    comando = f'python chatDevCore.py  --task "{task}" --name "{name}"'
+    #comando = f'python chatDevCore.py  --task "{task}" --name "{name}"'
 
     #print(comando)
 
-    os.system(comando)
+    #os.system(comando)
     
     return 'OK', 200
 
@@ -38,11 +37,23 @@ def aidi_create():
     
     # Crear un course config
     if target == "course-setting" and action == "create":
-        res = course_setting_generate(contentido)
+        ##res = course_setting_generate(contentido)
         print(res)
         #crearFirstPrompt()
 
     return 'OK', 200
+
+
+def create_prompt_chat_dev( core , config , body ):
+
+    res = ""
+
+    for name, value in body.items():
+        res = res + f"--{name} '{value}' "
+    
+    response = f"python {core} --config '{config}' {res}"
+
+    return response
 
 
 def course_setting_generate(prompt):
@@ -53,24 +64,30 @@ def course_setting_generate(prompt):
           El curso se creare en el idioma: '{idioma}'.
           La modalidad del curso sera: '{curso_modalidad}'.
 
+
           ¿Optimiza de forma masiva el aprendisaje en la plataforma? '{mooc}'.
-          ¿Optimiza el curso para aprendizaje basado en grupos pequeño? '{project_bases}'.
-          ¿Generar prototipos de tareas para posterior elaboracion? '{task_prototype}'.
+          ¿Optimiza el curso para aprendizaje basado en grupos pequeño? '{project_based}'.
+          ¿Generar prototipos de tareas para posterior elaboracion? '{task_prototypes}'.
+
 
           El curso durara {weeks} semanas.
           El nivel del curso sera {course_level}
           """
     res = PromptTemplate.from_template(template)
 
-    formatted_prompt = res.format( de_que_trata=prompt["de_que_trata"], 
+
+    formatted_prompt = res.format( de_que_trata=prompt["de_que_trata"],
                                    participantes_y_metas=prompt["participantes_y_metas"],
                                    conocmientos_base=prompt["conocmientos_base"],
-                                   curso_modalidad=prompt["modalidad"],
+                                   curso_modalidad=prompt["curso_modalidad"],
                                    idioma=prompt["idioma"],
                                    mooc=prompt["mooc"],
-                                   project_bases=prompt["project_bases"],
-                                   task_prototype=prompt["task_prototype"],
+                                   project_based=prompt["project_based"],
+                                   task_prototypes=prompt["task_prototypes"],
                                    weeks=prompt["weeks"],
                                    course_level=prompt["course_level"])
-    
+   
     return formatted_prompt
+
+
+
