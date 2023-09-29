0249d0fc5116c47f4c69073b2ee9a0543861b3aa
Jeremías San Martín <jerexxypunto@gmail.com>
2023-09-29 16:15:55 -0300

    aider: Modified `course_setting_generate` function to make the template string a single line string, replacing the newlines with escaped newline characters.
    # Aider chat conversation:
    
    ASSISTANT: Sure, I will modify the `course_setting_generate` function in the `api.py` file to make the template string a single line string, replacing the newlines with escaped newline characters (`\n`).
    
    Here is the change:
    
    ```python
    api.py
    <<<<<<< HEAD
        template = """
              El curso trata sobre '{de_que_trata}'.
              Los participantes de curso y sus metas son: '{participantes_y_metas}'.
              Los conocmientos necesarios antes de tomar el curso son: '{conocmientos_base}'.
              El curso se creare en el idioma: '{idioma}'.
              La modalidad del curso sera: '{curso_modalidad}'.
    
              ¿Optimiza de forma masiva el aprendisaje en la plataforma? '{mooc}'.
              ¿Optimiza el curso para aprendizaje basado en grupos pequeño? '{project_based}'.
              ¿Generar prototipos de tareas para posterior elaboracion? '{task_prototypes}'.
    
              El curso durara {weeks} semanas.
              El nivel del curso sera {course_level}
              """
    =======
        template = "El curso trata sobre '{de_que_trata}'.\nLos participantes de curso y sus metas son: '{participantes_y_metas}'.\nLos conocmientos necesarios antes de tomar el curso son: '{conocmientos_base}'.\nEl curso se creare en el idioma: '{idioma}'.\nLa modalidad del curso sera: '{curso_modalidad}'.\n\n¿Optimiza de forma masiva el aprendisaje en la plataforma? '{mooc}'.\n¿Optimiza el curso para aprendizaje basado en grupos pequeño? '{project_based}'.\n¿Generar prototipos de tareas para posterior elaboracion? '{task_prototypes}'.\n\nEl curso durara {weeks} semanas.\nEl nivel del curso sera {course_level}"
    >>>>>>> updated
    ```

--- api.py
+++ api.py
@@ -57,22 +57,7 @@ def create_prompt_chat_dev( core , config , body ):
 
 
 def course_setting_generate(prompt):
-    template = """
-          El curso trata sobre '{de_que_trata}'.
-          Los participantes de curso y sus metas son: '{participantes_y_metas}'.
-          Los conocmientos necesarios antes de tomar el curso son: '{conocmientos_base}'.
-          El curso se creare en el idioma: '{idioma}'.
-          La modalidad del curso sera: '{curso_modalidad}'.
-
-
-          ¿Optimiza de forma masiva el aprendisaje en la plataforma? '{mooc}'.
-          ¿Optimiza el curso para aprendizaje basado en grupos pequeño? '{project_based}'.
-          ¿Generar prototipos de tareas para posterior elaboracion? '{task_prototypes}'.
-
-
-          El curso durara {weeks} semanas.
-          El nivel del curso sera {course_level}
-          """
+    template = "El curso trata sobre '{de_que_trata}'.\nLos participantes de curso y sus metas son: '{participantes_y_metas}'.\nLos conocmientos necesarios antes de tomar el curso son: '{conocmientos_base}'.\nEl curso se creare en el idioma: '{idioma}'.\nLa modalidad del curso sera: '{curso_modalidad}'.\n\n¿Optimiza de forma masiva el aprendisaje en la plataforma? '{mooc}'.\n¿Optimiza el curso para aprendizaje basado en grupos pequeño? '{project_based}'.\n¿Generar prototipos de tareas para posterior elaboracion? '{task_prototypes}'.\n\nEl curso durara {weeks} semanas.\nEl nivel del curso sera {course_level}"
     res = PromptTemplate.from_template(template)
 
 
